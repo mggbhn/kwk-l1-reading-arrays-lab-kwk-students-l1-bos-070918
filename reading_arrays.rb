defe91fa2148c0265445dcaf70886bd4b606891a
mggbhn <maggiebswim@gmail.com>
2018-07-11 14:32:57 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -13,12 +13,15 @@ end
 first_student_by_index
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  puts STUDENT_NAMES[3]
 end
+fourth_student_by_index
 
 def last_student_by_index
+  puts STUDENT_NAMES.last
   # Write a solution that returns the first student in the array student_names
 end
+last_student_by_index
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
